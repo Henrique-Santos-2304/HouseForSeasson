fc818f1c3cb2d4576d691dd616404cdb0591b9c3
Henrique <henrique.multitech@gmail.com>
2022-05-26 08:55:38 -0300

    choore: create activities MyAccount, FilterAnnouncement

---
+++ app/src/main/java/com/example/multitech/houseforseasson/activitys/FilterAnnoucementActivity.java
@@ -0,0 +1,16 @@
+package com.example.multitech.houseforseasson.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multitech.houseforseasson.R;
+
+public class FilterAnnoucementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_annoucement);
+    }
+}
---
+++ app/src/main/java/com/example/multitech/houseforseasson/activitys/MyAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.multitech.houseforseasson.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multitech.houseforseasson.R;
+
+public class MyAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
