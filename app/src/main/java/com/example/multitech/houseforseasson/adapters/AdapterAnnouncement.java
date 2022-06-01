d6ca26d06b95d099dd66ef16d689c4f83825ea6f
henrique <henrique.multitech@gmail.com>
2022-05-31 23:43:12 -0300

    choore: add item announcement list

---
+++ app/src/main/java/com/example/multitech/houseforseasson/adapters/AdapterAnnouncement.java
@@ -0,0 +1,68 @@
+package com.example.multitech.houseforseasson.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.multitech.houseforseasson.R;
+import com.example.multitech.houseforseasson.database.models.Announcement;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterAnnouncement  extends RecyclerView.Adapter<AdapterAnnouncement.AnnouncementViewHolder> {
+    private final List<Announcement> announcementList;
+    private final Onclick onclick;
+
+    public AdapterAnnouncement(List<Announcement> announcementList, Onclick onclick) {
+        this.announcementList = announcementList;
+        this.onclick = onclick;
+    }
+
+    @NonNull
+    @Override
+    public AnnouncementViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.annoucement_item_list,parent, false);
+        return new AnnouncementViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnnouncementViewHolder holder, int position) {
+        Announcement announmcement = this.announcementList.get(position);
+
+        String announcementTitle = announmcement.getTitle();
+        String announcementDescription = announmcement.getDescription();
+//        String announcementDate = announmcement.;
+        Picasso.get().load(announmcement.getUrlImg()).into(holder.img_announcement);
+        holder.title.setText(announcementTitle);
+        holder.description.setText(announcementDescription);
+        holder.date.setText("23/04/1992 às 19:45");
+
+        holder.itemView.setOnClickListener(view -> {
+            this.onclick.OnClickeListener(announmcement);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.announcementList.size();
+    }
+
+    static class AnnouncementViewHolder extends RecyclerView.ViewHolder {
+        private TextView title,description,date;
+        private ImageView img_announcement;
+        public AnnouncementViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.title = itemView.findViewById(R.id.item_title_announcement);
+            this.description = itemView.findViewById(R.id.item_description_announcement);
+            this.date = itemView.findViewById(R.id.item_date_announcement);
+            this.img_announcement = itemView.findViewById(R.id.item_img_annoucement);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/multitech/houseforseasson/adapters/Onclick.java
@@ -0,0 +1,7 @@
+package com.example.multitech.houseforseasson.adapters;
+
+import com.example.multitech.houseforseasson.database.models.Announcement;
+
+public interface Onclick {
+    void OnClickeListener(Announcement announcement);
+}
